a59b286f7bfc2228f43817f5a66bc2b62ba62120
Aleksei Allen <aqml@mail.ru>
2023-06-08 01:07:35 +0500

    Lab13 corrected

--- Lab13.Task1.SchoolPolym/main.cpp
+++ Lab13.Task1.SchoolPolym/main.cpp
@@ -8,19 +8,7 @@
 int main()
 {
 	system("chcp 1251");
-
-	// Оценкистудента
-	std::vector<int> scores;
-	// Добавление оценок студента в вектор
-	scores.push_back(5);
-	scores.push_back(3);
-	scores.push_back(4);
-	scores.push_back(4);
-	scores.push_back(5);
-	scores.push_back(3);
-	scores.push_back(3);
-	scores.push_back(3);
-	scores.push_back(3);
+	
 	/*student* stud = new student("Петров", "Иван", "Алексеевич", scores);
 	stud->get_person_data();
 
@@ -28,18 +16,56 @@ int main()
 	teacher* tch = new teacher("Сергеев", "Дмитрий", "Сергеевич", teacher_work_time);
 	tch->get_person_data();*/
 
-	human man("Иванов", "Иван", "Иванович");
+	/*human man("Иванов", "Иван", "Иванович");
 	student student("Петров", "Иван", "Алексеевич", scores);
-	teacher teacher("Сергеев", "Дмитрий", "Сергеевич", 40);;
+	teacher teacher("Сергеев", "Дмитрий", "Сергеевич", 40);;*/
 
-	human* poly_man = &man;
+	/*human* poly_man = &man;
 	poly_man->get_person_data();
 
 	poly_man = (human*)&student;
 	poly_man->get_person_data();
 
 	poly_man = (human*)&teacher;
-	poly_man->get_person_data();
+	poly_man->get_person_data();*/
 
+	human* poly_man[10];
+	int n = 0;
+	char choice;
+	do
+	{
+		std::cout << "Вводить данные для студента или учителя (s / t) ? ";
+		std::cin >> choice;
+
+		if (choice == 's')
+		{
+			//Оценки студента
+			std::vector<int> scores;
+			//Добавление оценок студента в вектор
+			scores.push_back(5);
+			scores.push_back(3);
+			scores.push_back(4);
+			scores.push_back(4);
+			scores.push_back(5);
+			scores.push_back(3);
+			scores.push_back(3);
+			scores.push_back(3);
+			scores.push_back(3);
+
+			poly_man[n] = new student("Петров", "Иван", "Алексеевич", scores);
+		}
+		else
+		{
+			poly_man[n] = new teacher("Сергеев", "Дмитрий", "Сергеевич", 40);
+		}
+		n++;
+		std::cout << "Продолжать((у / n) ? ";
+		std::cin >> choice;
+	} while (choice == 'y');
+
+	for (int j = 0; j <= n; j++) //цикл по всем объектам
+		poly_man[j]->get_person_data(); //вывести данные о человеке
+	std::cout << std::endl;
+	
 	return 0;
 }
